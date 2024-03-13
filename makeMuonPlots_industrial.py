b1a06baa40bda9c4152fe3cabafaecb91176c874
Alexander Tuna <alexander.tuna@gmail.com>
2024-03-13 17:48:10 -0500

    Type hinting

--- makeMuonPlots_industrial.py
+++ makeMuonPlots_industrial.py
@@ -1,35 +1,24 @@
-import pyLCIO
+import pyLCIO # type: ignore
 import glob
 import math
 import os
 
 import numpy as np
-import pandas as pd
-import matplotlib as mpl
+import pandas as pd # type: ignore
+import matplotlib as mpl # type: ignore
 mpl.use('Agg')
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 
-def main():
+from typing import List
 
-    # Gather input files
+def main() -> None:
     filepath = "/data/fmeloni/DataMuC_MuColl10_v0A/reco/muonGun_pT_0_50/*.slcio"
     fnames = glob.glob(filepath)
+    df = getData(fnames)
+    plot(df)
 
-    # Read LCIO into pandas
-    pqname = 'makeMuonPlots_industrial.parquet'
-    pkname = 'makeMuonPlots_industrial.pickle'
-    if os.path.exists(pqname):
-        df = pd.read_parquet(pqname)
-    else:
-        df = processFiles(fnames)
-        postProcess(df)
-        print(df)
-        print('Writing to file ...')
-        # pip install pyarrow
-        df.to_parquet(pqname)
-        df.to_pickle(pkname)
 
-    # Plot
+def plot(df: pd.DataFrame) -> None:
     print('Plotting ...')
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), constrained_layout=True)
     bins = np.arange(0, 100, 1)
@@ -43,10 +32,30 @@ def main():
         ax[i_ax].tick_params(right=True, top=True)
     plt.savefig('industry.pdf')
 
-def postProcess(df):
-    df['pt'] = np.linalg.norm(df[['px', 'py']], axis=1)
 
-def processFiles(fnames):
+def getData(fnames: List[str]) -> pd.DataFrame:
+    # Read LCIO into pandas
+    pqname = 'makeMuonPlots_industrial.parquet'
+    pkname = 'makeMuonPlots_industrial.pickle'
+    if os.path.exists(pqname):
+        df = pd.read_parquet(pqname)
+    else:
+        df = processFiles(fnames)
+        postProcess(df)
+        print(df)
+        print('Writing to file ...')
+        # pip install pyarrow
+        df.to_parquet(pqname)
+        df.to_pickle(pkname)
+    return df
+
+
+def postProcess(df: pd.DataFrame) -> None:
+    df['pt'] = np.linalg.norm(df[['px', 'py']], axis=1)
+    df['ismuon'] = isMuon(df['type'])
+
+
+def processFiles(fnames: List[str]) -> pd.DataFrame:
     n_rows = 0
     dfs = []
     for i_fname, f in enumerate(fnames):
@@ -57,42 +66,46 @@ def processFiles(fnames):
         #     break
     return pd.concat(dfs, ignore_index=True)
 
-def processFile(fname):
+
+def processFile(fname: str) -> pd.DataFrame:
     reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
     reader.open(fname)
     return processEvents(reader)
 
-def processEvents(reader):
+
+def processEvents(reader) -> pd.DataFrame:
     n_rows = 0
     dfs = []
     for i_event, event in enumerate(reader):
         dfs.append( processEvent(event) )
     return pd.concat(dfs)
 
-def processEvent(event):
+
+def processEvent(event) -> pd.DataFrame:
     pfos = event.getCollection("PandoraPFOs")
     n_rows = len(pfos)
     df = createDataframe(n_rows)
     for row, pfo in enumerate(pfos):
         pfo_p = pfo.getMomentum()
-        df.at[row, 'px'] = pfo_p[0]
-        df.at[row, 'py'] = pfo_p[1]
-        df.at[row, 'pz'] = pfo_p[2]
+        df.at[row, 'px']   = pfo_p[0]
+        df.at[row, 'py']   = pfo_p[1]
+        df.at[row, 'pz']   = pfo_p[2]
         df.at[row, 'type'] = pfo.getType()
-        df.at[row, 'ismuon'] = isMuon(pfo.getType())
     return df
 
-def createDataframe(n_rows):
+
+def createDataframe(n_rows: int) -> pd.DataFrame:
     return pd.DataFrame({
         'px':     np.zeros(n_rows, dtype=float),
         'py':     np.zeros(n_rows, dtype=float),
         'pz':     np.zeros(n_rows, dtype=float),
         'type':   np.zeros(n_rows, dtype=int),
-        'ismuon': np.zeros(n_rows, dtype=bool),
     })
 
-def isMuon(val):
+
+def isMuon(val: int) -> int:
     return np.abs(val) == 13
+
 
 if __name__ == '__main__':
     main()
